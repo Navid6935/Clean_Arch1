c5dd08a23aab846cc37d5f0aa7a9b7679a1f4d72
NaviD <NaviD@DESKTOP-ILMPI5P>
2020-09-01 23:30:47 +0430

    990611

---
+++ CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Models;
+using CleanArch.Domain.Models.Interfaces;
+using CleanArch.Indra.Data.Repository;
+
+namespace CleanArch.Application.Services
+{
+
+    public class CourseService : ICourseService
+    {
+        private ICourseRepository _courseRepository;
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+        public CourseViewModel GetCources()
+        {
+            return new CourseViewModel()
+            {
+                Courses = _courseRepository.GetCourses()
+            };
+        }
+
+
+    }
+}
---
+++ CleanArch.Indra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Domain.Models;
+using CleanArch.Domain.Models.Interfaces;
+using CleanArch.Indra.Data.Context;
+
+namespace CleanArch.Indra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UnivercityDBContext _context;
+
+        public CourseRepository(UnivercityDBContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
